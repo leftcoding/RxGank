42a0e079cc085d53541b3d1a1653f0fe77f9bb03
leftcoding <leftcoding@gmail.com>
2019-05-10 18:02:54 +0800

    module 更改统一引用库

--- support-coding-file/src/main/java/com/coding/file/PathFileUtils.java
+++ support-coding-file/src/main/java/com/coding/file/PathFileUtils.java
@@ -1,6 +1,9 @@
 package com.coding.file;
 
 import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
 
 public class PathFileUtils {
     private PathFileUtils() {
@@ -8,6 +11,8 @@ public class PathFileUtils {
     }
 
     private static void init(Context context) {
-
+        context = context.getApplicationContext() == null ? context : context.getApplicationContext();
+        String packageName = context.getPackageName();
+        File appDir = new File(Environment.getExternalStorageDirectory(), "GankLy/network");
     }
 }
